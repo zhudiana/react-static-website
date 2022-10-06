99b899e709a0ba881173ac099fb1be4f23cbd951
Yodit Fentaw <yoditfentaw90@gmail.com>
2022-10-06 01:46:10 -0700

    --

--- my-app/src/Components/Home/Body/Body1.js
+++ my-app/src/Components/Home/Body/Body1.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./Body.css";
 
 const Body1 = () => {
-  return <div className="main-body1"></div>;
+  return (
+    <div className="main-body1">
+      <div className="sub-title">EcoSmart Cargo</div>
+    </div>
+  );
 };
 
 export default Body1;
